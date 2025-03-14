b23b354520695570c36eb8a41196dcb2600b6408
noname <no@email.com>
2025-03-14 12:06:44 +0100

    Names of graphs

---
+++ README.md
@@ -0,0 +1,6 @@
+# Code for EE3P11 lab report
+
+`makegraphs.py` makes plots from the CSV files under
+`data` and puts them into `output`
+
+
--- makegraphs.py
+++ makegraphs.py
@@ -18,33 +18,68 @@ scatter_args = dict(
     linewidth=0
     )
 
-titles = {
-    "TEK0001": "Foobar",
-    }
+def readcsv(stem):
+    csvfile = csvdir / f"{stem}.CSV"
+    df = pd.read_csv(csvfile, skiprows=range(0, 19), header=None)
+    df = df.iloc[:, 3:5]
+    df.columns=['x', 'y']
+    return df
+
+ds_baseline_ch1 = readcsv('TEK0000')
+ds_baseline_ch2 = readcsv('TEK0001')
+ds_long_ch1 = readcsv('TEK0002')
+ds_long_ch2 = readcsv('TEK0003')
+ds_open = readcsv('TEK0004')
+ds_short = readcsv('TEK0005')
+ds_matched = readcsv('TEK0006')
+ds_black = readcsv('TEK0007')
+ds_gray = readcsv('TEK0008')
 
 outdir.mkdir(exist_ok=True, parents=True)
 
-for csvfile in csvdir.rglob('*.CSV'):
-
-    stem = csvfile.stem
-    log(f"Making plot for {stem}")
-    if stem not in titles.keys():
-        log(f"Not title specified!")
-
-    # read csv into datafram
-    df = pd.read_csv(csvfile, skiprows=range(0, 19), header=None)
-
-    # Get dataserieses from cols 3, 4
-    ds1 = df.iloc[:, 3]
-    ds2 = df.iloc[:, 4]
-
+def makegraph(builder, stem):
     fig, ax = plt.subplots(1)
-    ax.scatter(ds1, ds2, **scatter_args)
-    fig.suptitle(titles.get(stem, stem))
-    ax.set_xlabel('Time [???]')
+    builder(fig, ax)
+    ax.set_xlabel('Time [Seconds]')
     ax.set_ylabel('Voltage [V]')
     fig.savefig(outdir / f"{stem}.pdf")
     fig.savefig(outdir / f"{stem}.svg")
 
+def build_baseline(fig, ax):
+    ax.scatter(ds_baseline_ch1['x'], ds_baseline_ch1['y'], **scatter_args)
+    ax.scatter(ds_baseline_ch2['x'], ds_baseline_ch2['y'], **scatter_args)
+    fig.suptitle("Baseline")
 
+def build_long(fig, ax):
+    ax.scatter(ds_long_ch1['x'], ds_long_ch1['y'], **scatter_args)
+    ax.scatter(ds_long_ch2['x'], ds_long_ch2['y'], **scatter_args)
+    fig.suptitle("Long cable")
+
+def build_open(fig, ax):
+    ax.scatter(ds_open['x'], ds_open['y'], **scatter_args)
+    fig.suptitle("Open Circuit")
+
+def build_short(fig, ax):
+    ax.scatter(ds_short['x'], ds_short['y'], **scatter_args)
+    fig.suptitle("Short Circuit")
+
+def build_matched(fig, ax):
+    ax.scatter(ds_matched['x'], ds_matched['y'], **scatter_args)
+    fig.suptitle("Matched Load")
+
+def build_black(fig, ax):
+    ax.scatter(ds_black['x'], ds_black['y'], **scatter_args)
+    fig.suptitle('"Black" Load')
+
+def build_gray(fig, ax):
+    ax.scatter(ds_gray['x'], ds_gray['y'], **scatter_args)
+    fig.suptitle('"Gray" Load')
+
+makegraph(build_baseline, 'baseline')
+makegraph(build_long, 'long')
+makegraph(build_open, 'open')
+makegraph(build_short, 'short')
+makegraph(build_matched, 'matched')
+makegraph(build_black, 'black')
+makegraph(build_gray, 'gray')
 

ea273056de69b248831908d63858e8ec3ea6a835
noname <no@email.com>
2025-03-14 13:03:02 +0100

    Time delay calculation

--- makegraphs.py
+++ makegraphs.py
@@ -32,7 +32,22 @@ ds_matched = readcsv('TEK0006')
 ds_black = readcsv('TEK0007')
 ds_gray = readcsv('TEK0008')
 
+
 outdir.mkdir(exist_ok=True, parents=True)
+
+def timedelay(ref, prop):
+    ref_10percent = (ref.y[len(ref.y) - 1] - ref.y[0]) * 0.1
+    prop_10percent = (prop.y[len(prop.y) - 1] - prop.y[0]) * 0.1
+
+    ref_index = (ref.y - ref_10percent).abs().argmin()
+    prop_index = (prop.y - prop_10percent).abs().argmin()
+
+    ref_time = ref.x[ref_index]
+    prop_time = prop.x[prop_index]
+
+    delta_time = prop_time - ref_time
+
+    return ref_index, prop_index, ref_time, prop_time
 
 def makegraph(builder, stem):
     fig, ax = plt.subplots(1)
@@ -43,6 +58,10 @@ def makegraph(builder, stem):
     fig.savefig(outdir / f"{stem}.svg")
 
 def build_baseline(fig, ax):
+    ref_index, prop_index, ref_time, prop_time = timedelay(ds_baseline_ch2, ds_baseline_ch1)
+
+    log(f'Baseline Time delay: {prop_time - ref_time:.6E} s')
+
     ax.plot(
             ds_baseline_ch2['x'],
             ds_baseline_ch2['y'],
@@ -55,10 +74,25 @@ def build_baseline(fig, ax):
             label='Propagated signal',
             **plot_args
             )
+    ax.axvline(ref_time, -1, 1, color='green')
+    ax.axvline(prop_time, -1, 1, color='green')
+    ax.annotate(
+        f'Time delay:\n{prop_time - ref_time:.3E} s',
+        xy = ((prop_time - ref_time) / 2, ds_baseline_ch2.y[ref_index]), 
+        xytext = (-150, 100),
+        textcoords='offset pixels',
+        fontsize = 12,
+        arrowprops = dict(facecolor = 'red'),
+        color = 'g'
+        )
     fig.suptitle("Baseline")
     ax.legend()
 
 def build_long(fig, ax):
+    ref_index, prop_index, ref_time, prop_time = timedelay(ds_long_ch2, ds_long_ch1)
+
+    log(f'Long Time delay: {prop_time - ref_time:.6E} s')
+
     ax.plot(
             ds_long_ch2['x'],
             ds_long_ch2['y'],
@@ -71,6 +105,15 @@ def build_long(fig, ax):
             label='Propagated signal',
             **plot_args,
             )
+    ax.axvline(ref_time, -1, 1, color='green')
+    ax.axvline(prop_time, -1, 1, color='green')
+    ax.text(
+        s=f'Time delay:\n{prop_time - ref_time:.3E} s',
+        x = (prop_time - ref_time) / 2,
+        y = ds_long_ch2.y[ref_index],
+        fontsize = 12,
+        color = 'g'
+        )
     fig.suptitle("Long cable")
     ax.legend()
 
